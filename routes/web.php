38f62875a504f8132c595af910cc105d387bc100
Carlos2396 <carlosaam96@gmail.com>
2018-10-31 15:00:40 -0600

    Added needed routes with auth middleware.

--- routes/web.php
+++ routes/web.php
@@ -11,10 +11,15 @@
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
 Auth::routes();
 
-Route::get('/home', 'HomeController@index')->name('home');
+Route::get('/', function() {
+    return redirect(route('coparisons'));
+})->name('home');
+
+Route::group(['middleware' => 'auth'], function() {
+    Route::get('/comparaciones', 'ComparisonController@index')->name('comparisons');
+    Route::get('/comparaciones/create', 'ComparisonController@create')->name('comparisons.create');
+    Route::get('/comparaciones/{comparison}', 'ComparisonController@show')->name('comparisons.show');
+    Route::post('/comparaciones', 'ComparisonController@store')->name('comparisons.store');
+});
