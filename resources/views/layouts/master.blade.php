ac9579c57df68d885c5a0ad54054096dd9b65d1b
Carlos Amador <carlosaam96@gmail.com>
2018-11-21 03:19:43 -0600

    Added Logout button in navbar.

--- resources/views/layouts/master.blade.php
+++ resources/views/layouts/master.blade.php
@@ -41,21 +41,19 @@
                 <a class="nav-link" href="{{ route('register') }}">Registrarse</a>
             @endif
         @else
-            <a id="navbarDropdown" class="nav-link dropdown-toggle" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
-                {{ Auth::user()->name }} <span class="caret"></span>
+            <a id="navbarDropdown" class="nav-link" style="margin-left:10px;" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+                Hola!, {{ Auth::user()->name }}
             </a>
 
-            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
-                <a class="dropdown-item" href="{{ route('logout') }}"
-                    onclick="event.preventDefault();
-                                    document.getElementById('logout-form').submit();">
-                    {{ __('Logout') }}
-                </a>
+            <a class="btn btn-outline-primary" style="margin-left: 50px;" href="{{ route('logout') }}"
+                onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                Cerrar sesión
+            </a>
 
                 <form id="logout-form" action="{{ route('logout') }}" method="POST" style="display: none;">
                     @csrf
                 </form>
-            </div>
         @endguest
     </div>
 
